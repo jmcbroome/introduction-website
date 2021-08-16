09ce2c637b09ab74e55abe382c38e9e8b8cf2e16
Alex Kramer <alex.kramer@ucsc.edu>
2021-08-16 14:29:42 -0700

    change to main

---
+++ src/main.js
@@ -0,0 +1,28 @@
+var map = L.map('mapid').setView([37.8, -96], 4);
+
+// Map values from geoJSON to a color 
+function getColor(d) {
+    return d > 1000 ? '#800026' :
+           d > 500  ? '#BD0026' :
+           d > 200  ? '#E31A1C' :
+           d > 100  ? '#FC4E2A' :
+           d > 50   ? '#FD8D3C' :
+           d > 20   ? '#FEB24C' :
+           d > 10   ? '#FED976' :
+                      '#FFEDA0';
+}
+
+function style(feature) {
+    return {
+        fillColor: getColor(feature.properties.density),
+        weight: 2,
+        opacity: 1,
+        color: 'white',
+        dashArray: '3',
+        fillOpacity: 0.7
+    };
+}
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png?{foo}', {foo: 'bar', attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'}).addTo(map);
+L.geoJson(statesData).addTo(map);
+L.geoJson(statesData, {style: style}).addTo(map);
