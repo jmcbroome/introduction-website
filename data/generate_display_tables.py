dd7d745720e53e0f000ed5692b698b92acd1786d
Jakob McBroome <jmcbroome@gmail.com>
2021-08-25 18:26:57 -0600

    First attempt at direct auspice fetch linking of top clusters

--- data/generate_display_tables.py
+++ data/generate_display_tables.py
@@ -7,6 +7,8 @@ conversion = {"AL":"Alabama","AK":"Alaska","AR":"Arkansas","AZ":"Arizona","CA":"
 "ND":"North Dakota","OH":"Ohio","OK":"Oklahoma","OR":"Oregon","PA":"Pennsylvania","RI":"Rhode Island",
 "SC":"South Carolina","SD":"South Dakota","TN":"Tennessee","TX":"Texas","UT":"Utah","VT":"Vermont","VA":"Virginia",
 "WA":"Washington","WV":"West Virginia","WI":"Wisconsin","WY":"Wyoming","PR":"Puerto Rico"}
+#this will need to be edited on migrating to a proper host service.
+host = "raw.githubusercontent.com/jmcbroome/introduction-website/main/"
 
 default_lines = {}
 with open("hardcoded_clusters.tsv") as inf:
@@ -35,16 +37,35 @@ with open("hardcoded_clusters.tsv") as inf:
             default_lines[float(spent[4])] = entry.strip()
             assert len(default_lines) == 5
 
-header = "Cluster ID\tRegion\tSample Count\tEarliest Date\tLatest Date\tClade/Lineage\tInferred Origins\tInferred Origin Confidences"
+header = "Cluster ID\tRegion\tSample Count\tEarliest Date\tLatest Date\tClade/Lineage\tInferred Origins\tInferred Origin Confidences\tLink to View"
+sout = open("extraction_targets.txt","w+")
+mout = open("cluster_labels.tsv","w+")
 for reg, lines in filelines.items():
     with open("display_tables/" + reg + "_topclusters.tsv", "w+") as outf:
         print(header,file=outf)
         for l in lines:
+            #save matching results to the other output files
+            #for downstream extraction of json
+            samples = spent[-1].split(",")
+            print(samples[0],file=sout)
+            for s in samples:
+                print(s + "\t" + spent[0],file=mout)
             #process the line 
             #into something more parseable.
             spent = l.split("\t")
-            outline = [spent[0], spent[9], spent[1], spent[2], spent[3], spent[12], spent[10], spent[11]]
+            #generate a link to exist in the last column
+            #based on the global "host" variable.
+            #and including all html syntax.
+            link = '<a href="'
+            link += "https://nextstrain.org/fetch/" + host + "data/display_json/"
+            link += spent[-1].split(',')[0].replace("/","_") 
+            link += "_context.json?c=" + spent[0]
+            link += '">View ' + spent[0] + "</a>"
+            outline = [spent[0], spent[9], spent[1], spent[2], spent[3], spent[12], spent[10], spent[11], link]
             print("\t".join(outline),file=outf)
+
+sout.close()
+mout.close()
 sorted_default_keys = sorted(list(default_lines.keys()),reverse=True)
 with open("display_tables/default_clusters.tsv","w+") as outf:
     print(header,file=outf)
