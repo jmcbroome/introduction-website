9dcac5422e845b84ee76976175b86e10c59a2451
Jakob McBroome <jmcbroome@gmail.com>
2021-08-25 18:52:28 -0600

    match rerendering

--- src/main.js
+++ src/main.js
@@ -97,7 +97,7 @@ function resetView(e) {
         element: 'table-container', 
         allow_download: false,
         csv_options: {separator: '\t', delimiter: '"'},
-        datatables_options: {"paging": false, "searching":false}
+        datatables_options: {"paging": false, "searching": false, "columnDefs": {"data":"Link to View","render":{function (data,type,row,meta) {return '<a href="' + data + '">View Cluster</a>'}}}}
     });
 }
 
